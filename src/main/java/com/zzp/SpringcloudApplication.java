aca2b0add9fa804c4ffe5f727ab4276493602b0e
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-03-26 22:43:28 +0800

    first step

--- src/main/java/com/zzp/SpringcloudApplication.java
+++ src/main/java/com/zzp/SpringcloudApplication.java
@@ -1,16 +1,24 @@
 package com.zzp;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
+@RestController
 @SpringBootApplication
 public class SpringcloudApplication {
 
+//	@RequestMapping(value = "/")
+//	public String location(){
+//		return "ShangHai";
+//
+//	}
+
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringcloudApplication.class, args);
-		new SpringApplicationBuilder(SpringcloudApplication.class).web(true).run(args);
+		SpringApplication.run(SpringcloudApplication.class, args);
+//		new SpringApplicationBuilder(SpringcloudApplication.class).web(true).run(args);
 	}
 
 }
