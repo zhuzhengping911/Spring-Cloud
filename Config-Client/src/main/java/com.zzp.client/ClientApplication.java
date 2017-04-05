833cff82d7302ce63e4ffaaa646eb573460c6308
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-06 01:38:08 +0800

    client config

---
+++ Config-Client/src/main/java/com.zzp.client/ClientApplication.java
@@ -0,0 +1,15 @@
+package com.zzp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhuzhengping on 2017/4/6.
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class,args);
+    }
+}
---
+++ Config-Client/src/main/java/com.zzp.client/controller/ClientController.java
@@ -0,0 +1,32 @@
+package com.zzp.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhuzhengping on 2017/4/6.
+ */
+@RestController
+@RefreshScope
+public class ClientController {
+
+    @Value("${zzp.from}")
+    private String from;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    @RequestMapping("/from")
+    public String from(){
+        return this.from;
+
+
+    }
+}
