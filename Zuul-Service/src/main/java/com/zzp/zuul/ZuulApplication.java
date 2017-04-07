6658fdaaccb2a07f5c5544382c89286484536e87
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-08 02:01:08 +0800

    zulu filter

--- Zuul-Service/src/main/java/com/zzp/zuul/ZuulApplication.java
+++ Zuul-Service/src/main/java/com/zzp/zuul/ZuulApplication.java
@@ -1,8 +1,10 @@
 package com.zzp.zuul;
 
+import com.zzp.zuul.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by zhuzhengping on 2017/4/6.
@@ -13,5 +15,10 @@ public class ZuulApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
