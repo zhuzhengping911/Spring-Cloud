c948e7c4b42c64b74aa0f5aa0911eedbcd1c1bc2
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-12 00:55:00 +0800

    spring bus

--- Config-Client/src/main/java/com.zzp.client/ClientApplication.java
+++ Config-Client/src/main/java/com.zzp.client/ClientApplication.java
@@ -1,14 +1,14 @@
 package com.zzp.client;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by zhuzhengping on 2017/4/6.
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
