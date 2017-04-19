6c873aef1aeca2d83120c4076620e97314e4298d
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-20 01:03:35 +0800

    add function

--- User-Service/src/main/java/com/zzp/user/UserApplication.java
+++ User-Service/src/main/java/com/zzp/user/UserApplication.java
@@ -1,5 +1,7 @@
 package com.zzp.user;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +9,9 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhuzhengping on 2017/3/31.
@@ -19,8 +24,22 @@ import org.springframework.context.annotation.ComponentScans;
 })
 @EnableHystrix
 @EnableHystrixDashboard
+@RestController
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
