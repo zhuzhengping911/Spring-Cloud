f511d5f3248b58f17ea73216b8d0fc140456979d
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-26 01:15:25 +0800

    remove

--- Computer-Service/src/main/java/com/zzp/computer/controller/ComputerController.java
+++ Computer-Service/src/main/java/com/zzp/computer/controller/ComputerController.java
@@ -2,24 +2,19 @@ package com.zzp.computer.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zzp.computer.dao.ComputerMapper;
-import com.zzp.computer.pojo.LearnResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by zhuzhengping on 2017/3/31.
  */
-@Controller
-//@RestController
+//@Controller
+@RestController
 public class ComputerController {
 
     @Autowired
@@ -27,22 +22,10 @@ public class ComputerController {
 
 
     @HystrixCommand(fallbackMethod = "error")
-    @RequestMapping(value = "/login")
-    public String index(ModelMap map){
-        map.addAttribute("author","zzp");
-        map.addAttribute("url","http://www.google.com");
-        map.addAttribute("title","blog");
-        return "index";
-    }
-
-    @RequestMapping("/t")
-    public ModelAndView index2(){
-
-        List<LearnResource> learnList =new ArrayList<LearnResource>();
-
-        ModelAndView modelAndView = new ModelAndView("/template");
-
-        return modelAndView;
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+//        return "login";
+        return new Date() + "";
     }
 
     public String error() {
