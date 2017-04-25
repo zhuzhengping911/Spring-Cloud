0a499c491e77ba8fef2ccaf836535cac76fab4eb
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-04-25 23:54:38 +0800

    add

--- Computer-Service/src/main/java/com/zzp/computer/controller/ComputerController.java
+++ Computer-Service/src/main/java/com/zzp/computer/controller/ComputerController.java
@@ -2,19 +2,23 @@ package com.zzp.computer.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zzp.computer.dao.ComputerMapper;
+import com.zzp.computer.pojo.LearnResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by zhuzhengping on 2017/3/31.
  */
-//@Controller
-@RestController
+@Controller
+//@RestController
 public class ComputerController {
 
     @Autowired
@@ -23,9 +27,60 @@ public class ComputerController {
 
     @HystrixCommand(fallbackMethod = "error")
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
-//        return "login";
-        return new Date() + "";
+    public ModelAndView index(){
+        List<LearnResource> learnList =new ArrayList<LearnResource>();
+        LearnResource bean =new LearnResource("官方参考文档","Spring Boot Reference Guide","http://docs.spring.io/spring-boot/docs/1.5.1.RELEASE/reference/htmlsingle/#getting-started-first-application");
+        learnList.add(bean);
+        bean =new LearnResource("官方SpriongBoot例子","官方SpriongBoot例子","https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples");
+        learnList.add(bean);
+        bean =new LearnResource("龙国学院","Spring Boot 教程系列学习","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResource("嘟嘟MD独立博客","Spring Boot干货系列 ","http://tengj.top/");
+        learnList.add(bean);
+        bean =new LearnResource("后端编程嘟","Spring Boot教程和视频 ","http://www.toutiao.com/m1559096720023553/");
+        learnList.add(bean);
+        bean =new LearnResource("程序猿DD","Spring Boot系列","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResource("纯洁的微笑","Sping Boot系列文章","http://www.ityouknow.com/spring-boot");
+        learnList.add(bean);
+        bean =new LearnResource("CSDN——小当博客专栏","Sping Boot学习","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResource("梁桂钊的博客","Spring Boot 揭秘与实战","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResource("林祥纤博客系列","从零开始学Spring Boot ","http://412887952-qq-com.iteye.com/category/356333");
+        learnList.add(bean);
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("learnList", learnList);
+        return modelAndView;
+    }
+
+    @RequestMapping("/t")
+    public ModelAndView index2(){
+
+        List<LearnResource> learnList =new ArrayList<LearnResource>();
+        LearnResource bean =new LearnResource("官方参考文档","Spring Boot Reference Guide","http://docs.spring.io/spring-boot/docs/1.5.1.RELEASE/reference/htmlsingle/#getting-started-first-application");
+        learnList.add(bean);
+        bean =new LearnResource("官方SpriongBoot例子","官方SpriongBoot例子","https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples");
+        learnList.add(bean);
+        bean =new LearnResource("龙国学院","Spring Boot 教程系列学习","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResource("嘟嘟MD独立博客","Spring Boot干货系列 ","http://tengj.top/");
+        learnList.add(bean);
+        bean =new LearnResource("后端编程嘟","Spring Boot教程和视频 ","http://www.toutiao.com/m1559096720023553/");
+        learnList.add(bean);
+        bean =new LearnResource("程序猿DD","Spring Boot系列","http://www.roncoo.com/article/detail/125488");
+        learnList.add(bean);
+        bean =new LearnResource("纯洁的微笑","Sping Boot系列文章","http://www.ityouknow.com/spring-boot");
+        learnList.add(bean);
+        bean =new LearnResource("CSDN——小当博客专栏","Sping Boot学习","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResource("梁桂钊的博客","Spring Boot 揭秘与实战","http://blog.csdn.net/column/details/spring-boot.html");
+        learnList.add(bean);
+        bean =new LearnResource("林祥纤博客系列","从零开始学Spring Boot ","http://412887952-qq-com.iteye.com/category/356333");
+        learnList.add(bean);
+        ModelAndView modelAndView = new ModelAndView("/template");
+        modelAndView.addObject("learnList", learnList);
+        return modelAndView;
     }
 
     public String error() {
---
+++ Computer-Service/src/main/java/com/zzp/computer/pojo/LearnResource.java
@@ -0,0 +1,42 @@
+package com.zzp.computer.pojo;
+
+/**
+ * Created by zhuzhengping on 2017/4/25.
+ */
+public class LearnResource {
+
+    private String author;
+    private String title;
+    private String url;
+
+    public LearnResource(String author, String title, String url) {
+        this.author = author;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getAuthor() {
+
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
