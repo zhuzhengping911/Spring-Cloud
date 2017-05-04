d831bf8cd968148455de67d137e9193c4eb42cc4
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-05 00:03:13 +0800

    add common function

--- Common-Service/src/main/java/com/zzp/common/CommonApplication.java
+++ Common-Service/src/main/java/com/zzp/common/CommonApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by zhuzhengping on 2017/3/30.
@@ -11,9 +13,26 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan("com.zzp.common")
+@Controller
 public class CommonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
     }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
+
+
