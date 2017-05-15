2791ef346ed8781f634eb2ab4641a4d65d3802de
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-15 23:49:52 +0800

    add encrypt

---
+++ Common-Service/src/main/java/com/zzp/common/util/Encrypt.java
@@ -0,0 +1,76 @@
+package com.zzp.common.util;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by zhuzhengping on 2017/5/15.
+ */
+public class Encrypt {
+
+
+    private static final String HMAC_SHA1 = "HmacSHA1";
+
+    /**
+     * 生成加密数据
+     *
+     * @param data
+     *            待加密的数据
+     * @param key
+     *            加密使用的key
+     * @throws NoSuchAlgorithmException
+     */
+    public static String getEncryptedData(String data, String key)
+            throws Exception {
+        byte[] keyBytes = key.getBytes();
+        SecretKeySpec signingKey = new SecretKeySpec(keyBytes, HMAC_SHA1);
+        Mac mac = Mac.getInstance(HMAC_SHA1);
+        mac.init(signingKey);
+        byte[] rawHmac = mac.doFinal(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : rawHmac) {
+            sb.append(byteToHexString(b));
+        }
+        return sb.toString();
+    }
+
+    private static String byteToHexString(byte ib) {
+        char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
+                'b', 'c', 'd', 'e', 'f' };
+        char[] ob = new char[2];
+        ob[0] = Digit[(ib >>> 4) & 0X0f];
+        ob[1] = Digit[ib & 0X0F];
+        String s = new String(ob);
+        return s;
+    }
+
+    public static String getMd5(String plainText) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(plainText.getBytes());
+            byte b[] = md.digest();
+
+            int i;
+
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            // 32位加密
+            return buf.toString();
+            // 16位的加密
+            // return buf.toString().substring(8, 24);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
