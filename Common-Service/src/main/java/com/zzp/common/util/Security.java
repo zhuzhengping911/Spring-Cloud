6d50cce09e1252452f01ee153c7a4f2193768c3b
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-16 23:55:23 +0800

    add security

---
+++ Common-Service/src/main/java/com/zzp/common/util/Security.java
@@ -0,0 +1,11 @@
+package com.zzp.common.util;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by zhuzhengping on 2017/5/16.
+ */
+public class Security {
+
+    private static final Logger logger = Logger.getLogger(Security.class);
+}
