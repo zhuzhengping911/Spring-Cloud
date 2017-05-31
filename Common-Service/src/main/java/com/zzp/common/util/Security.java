cb3a9d8cccd7bb91839b977a34766cb86e72b4f2
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-31 23:56:38 +0800

    add

--- Common-Service/src/main/java/com/zzp/common/util/Security.java
+++ Common-Service/src/main/java/com/zzp/common/util/Security.java
@@ -2,10 +2,38 @@ package com.zzp.common.util;
 
 import org.apache.log4j.Logger;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * Created by zhuzhengping on 2017/5/16.
  */
 public class Security {
 
     private static final Logger logger = Logger.getLogger(Security.class);
+
+    /**
+     * map按key排序取值
+     *
+     * @param param 参数
+     * @return 排序后的value拼接字符串
+     * @throws UnsupportedEncodingException
+     */
+    public static String parseStrBykey(Map<String, Object> param) throws UnsupportedEncodingException {
+
+        String str = "";
+
+        param.remove("signature");
+        Object[] key = param.keySet().toArray();
+        Arrays.sort(key);
+
+        int i = 0, len = key.length;
+        while (i < len) {
+            str += param.get(key[i++]);
+        }
+
+        return URLEncoder.encode(str, "UTF-8");
+    }
 }
